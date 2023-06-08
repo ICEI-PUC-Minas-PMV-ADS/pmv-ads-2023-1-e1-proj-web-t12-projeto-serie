a786ae832d7e6ca307ec15a63ebff0ca4ced8007
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2023-06-08 22:27:30 +0000

    Finalizando Tela Match

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -111,7 +111,7 @@ export default function SearchBar() {
             </div>
             </div>
             <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-red-600 border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 <span className="sr-only">Search</span>
             </button>
         </div>
